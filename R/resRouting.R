2498cfeb0ea7f5644f046ad80d98cfba0da6e5f2
Sophia Dobkowitz <dobkowitz@ad.geoecology.uni-potsdam.de>
2019-04-10 15:07:29 +0200

    work on reservoir routing

--- R/resRouting.R
+++ R/resRouting.R
@@ -2,6 +2,8 @@
 
 resRouting <- function(list_output){
 
+  library(sf)
+
   if(list_output$routing == F){
   print("no routing ")
     }else{
@@ -14,9 +16,9 @@ catch <- list_output$catch
       res_riv_catch <- subset(res_riv_catch, !is.na(ARCID))
 
       res_order <- NULL
-      ID <- unique(res_riv_catch$id_jrc)
-      for(i in 1:length(ID)){
-        res <- subset(res_riv_catch, id_jrc == ID[i])
+      IDs <- unique(res_riv_catch$id_jrc)
+      for(i in 1:length(IDs)){
+        res <- subset(res_riv_catch, id_jrc == IDs[i])
         riv <- unique(data.frame(ARCID = res$ARCID, UP_CELLS = res$UP_CELLS))
         otto <- unique(data.frame(HYBAS_ID = res$HYBAS_ID, SUB_AREA = res$SUB_AREA))
 
@@ -28,11 +30,12 @@ catch <- list_output$catch
         up_cells_km2 <- up_cells * (30.87 * cos(lat*2*pi/360)*15)^2/10^6
         sub_area_km2 <- sum(otto$SUB_AREA)
 
-        o <- data.frame(id_jrc =ID[i], up_cells_km2 = up_cells_km2, sub_area_km2 = sub_area_km2)
+        o <- data.frame(id_jrc = IDs[i], up_cells_km2 = up_cells_km2, sub_area_km2 = sub_area_km2)
         res_order <- rbind(res_order, o)
       }
 
       res_main <- subset(res_order, up_cells_km2 > 0.5 * sub_area_km2)
+      res_main$res_down <- NA
       res_not_main <- subset(res_order, up_cells_km2 <= 0.5 * sub_area_km2)
 
 
@@ -46,7 +49,37 @@ plot(res_riv_catch$geometry[res_riv_catch$id_jrc %in% res_main$id_jrc], add = T)
 for(r in 1:length(res_main)){
 res_down <- NULL
 res <- subset(res_riv_catch, id_jrc == res_main$id_jrc[r])
-otto_start <- res[res$UP_AREA == max(res$UP_AREA),]
+otto_start <- res[res$UP_AREA == max(res$UP_AREA),]$HYBAS_ID
+otto_start <- subset(catch, HYBAS_ID ==otto_start)
+otto_down <- catch[catch$HYBAS_ID == otto_start$NEXT_DOWN,]
+res_otto_start <- subset(res_riv_catch, id_jrc %in% res_main$id_jrc & HYBAS_ID == otto_start$HYBAS_ID & id_jrc != res_main$id_jrc[r])
+# downstream reservoirs have at least the same up_cells and if the same, they should be more near to the next downstream subbasin
+res_down <- subset(res_otto_start, UP_CELLS >= max(res$UP_CELLS))
+res_down <- res_down[as.numeric(st_distance(res_down, otto_down)) < as.numeric(st_distance(res, otto_down)),]
+
+  if(nrow(res_down) > 1){
+    # choose the reservoir with the smallest distance to res
+    res_main$res_down[r] <- res_down[st_distance(res_down,res) == min(st_distance(res_down, res)),]$id_jrc
+    }
+
+  if(nrow(res_down) == 1){
+    res_main$res_down[r] <- res_down$id_jrc
+  }
+  # go to the next downstream subbasin until finding a reservoir
+  if(nrow(res_down) == 0){
+    while(is.null(res_down)){
+
+    }
+    res_main$res_down[r] <- ...
+  }
+
+
+plot(catch[catch$HYBAS_ID == otto_start,]$geometry)
+plot(res_otto_start$geometry, col = "blue", add = T)
+plot(res$geometry, col = "darkred", add = T)
+plot(res_next_down$geometry, col = "green", add = T)
+# plot(catch[catch$HYBAS_ID == 6120357770,]$geometry, col = "green", add = T)
+# plot(res$geometry, col = "green", add = T)
 
 otto_down <- otto[otto$HYBAS_ID == otto_start$NEXT_DOWN,]
 res_down <- merge(res_main, res_riv_catch[res_riv_catch$HYBAS_ID == otto_down$NEXT_DOWN,], by = "id_jrc")
