f6e9fd78703947171a5a11866398b5ce585185ca
Sophia Dobkowitz <dobkowitz@ad.geoecology.uni-potsdam.de>
2019-05-09 13:54:03 +0200

    graph routing

--- R/routing_with_graph.R
+++ R/routing_with_graph.R
@@ -1,26 +1,61 @@
 # reservoir routing with river graph ####
+# library(dplyr)
+# riv %>% group_by(ARCID) %>% summarize(N=n()) %>% filter(N>1)
+library(sf)
 
-# identify leaves
+# get leaves ####
+g <- river_graph
+
+# leaves=which(degree(g, v = V(g), mode = "in")==0)
+# i=leaves[50]
+# for(i in leaves){
+# riv_down <- riv[neighbors(g,i,mode='out'),]
+# }
+
+leaves = which(degree(g, v = V(g), mode = "in")==0)
+riv_down <- data.frame(leaves = leaves, riv_down = NA)
+for(i in 1:length(leaves)){
+  rd <- as.numeric(neighbors(g,leaves[i],mode='out'))
+  if(length(rd) == 0){
+    riv_down$riv_down[i] <- NA
+  }else{
+    riv_down$riv_down[i] <- rd
+  }
+}
+
 
 # for-loop through leaves
-
 res_max$next_res <- NA
-strategic <- res_max[res_max$`distance to river`==0,]
-river_reaches <- unique(strategic$`nearest river`)
+# strategic <- res_max[res_max$`distance to river`==0,]
+# river_reaches <- unique(strategic$`nearest river`)
 
-points <- st_line_sample(riv[riv$ARCID==river_reaches[1],], n = 50)
-points <- st_cast(points, "POINT")
-points <- st_sf(points)
-points$sample <- 1:50
+for(l in 1:nrow(riv_down)){
+  points <- st_line_sample(riv[riv_down$leaves[l],], n = 50)
+  points <- st_cast(points, "POINT")
+  points <- st_sf(points)
+  points$sample <- 1:50
 
-plot(points)
+  res_l <- subset(res_max, `nearest river` == riv$ARCID[l])
+  if(nrow(res_l) > 0){
+    strat_l <- subset(res_l, `distance to river`==0)
 
-plot(points$points)
-plot(strategic$geometry[strategic$`nearest river`==river_reaches[1]], add = T, col = "cadetblue")
+    if(nrow(strat_l) > 1){
+      inter <- st_intersection(strat_l, points)
+      r <- data.frame(id_jrc = unique(inter$id_jrc))
+      for(i in 1:nrow(r)){
+        r$sample_max[i] <- max(inter$sample[inter$id_jrc== r$id_jrc[i]])
+      }
 
-inter <- st_intersection(strategic, points)
-r <- data.frame(id_jrc = unique(inter$id_jrc))
-for(i in 1:nrow(r)){
-  r$sample_max[i] <- max(inter$sample[inter$id_jrc== r$id_jrc[i]])
+    }
+
+  }
+
 }
+
+# plot(points)
+#
+plot(points$points)
+# plot(strategic$geometry[strategic$`nearest river`==river_reaches[1]], add = T, col = "cadetblue")
+
+plot(strat_l$geometry, add = T)
 
